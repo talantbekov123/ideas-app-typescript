dd28275fb2dcd15262b656417a95e88c1e6a5217
Talantbekov Kairat <talantbekov123@gmail.com>
2019-01-06 21:31:31 +0600

    CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+/* Controllers */
 import { AppController } from './app.controller';
+/* Services */
 import { AppService } from './app.service';
+import { IdeaModule } from './idea/idea.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), IdeaModule],
   controllers: [AppController],
   providers: [AppService],
 })
